e0d8c45bb50000ab1d0c93ecfc67a1b69718c9b4
Abel Begashaw <phenomenalironfist@gmail.com>
2025-03-21 06:44:12 +0300

    Modified the Cass to have spacing between each input fields

---
+++ Web Track/Task 1/main.js
@@ -0,0 +1,27 @@
+/**
+ * create the scripting logic here ... 
+ * 
+ * Define the initial tasks
+ * Good Idea to incorporate a small dashboard to show stats
+ * Incorporate a dropdown feature or a way to update the status of each tasks
+ * 
+ */
+
+// Define the initial tasks
+let tasks = [
+    {
+        id: 1,
+        task: 'This is the first task',
+        status: 'In Progress'
+    },
+    {
+        id: 2,
+        task: 'This is the second task',
+        status: 'In Progress'
+    },
+    {
+        id: 3,
+        task: 'This is the third task',
+        status: 'In Progress'
+    }
+];
